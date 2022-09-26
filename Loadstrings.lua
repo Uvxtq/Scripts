0b7d2ba45d5a8f485de778ec2e38090b3c2e2f46
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-09-26 18:08:20 -0400

    Update Loadstrings.lua

--- Loadstrings.lua
+++ Loadstrings.lua
@@ -5,4 +5,4 @@ loadstring(game:HttpGet(('https://raw.githubusercontent.com/Uvxtq/Scripts/main/g
 loadstring(game:HttpGet(('https://scripts.luawl.com/12655/Evade-Kaoru.lua')))('Kaoru~#6438');
 
 -- Anime Story:
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Uvxtq/Scripts/main/games/Anime%20Story')))('Kaoru~#6438');
+loadstring(game:HttpGet(('https://scripts.luawl.com/12654/AnimeStory-Kaoru.lua')))('Kaoru~#6438');
