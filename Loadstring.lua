9ff84369bb07cc2dc37fb7e9f5fff80e384bf437
Kaoru <96850544+Uvxtq@users.noreply.github.com>
2022-09-25 21:45:22 -0400

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,5 @@
+-- Universal:
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Uvxtq/Scripts/main/games/Universal.lua')))('Kaoru~#6438');
+
+-- Evade:
+loadstring(game:HttpGet('https://scripts.luawl.com/12655/Evade-Kaoru.lua'))()
